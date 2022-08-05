e0396a63a22f738777e35a251fd483a62d2ee426
Xianghui <xiexh98@gmail.com>
2022-08-05 22:47:00 +0200

    code to convert smpl parameters to mesh

---
+++ tools/smpl_params2mesh.py
@@ -0,0 +1,41 @@
+"""
+simple script to demonstrate converting smpl params to SMPL mesh
+"""
+import sys, os
+sys.path.append(os.getcwd())
+import pickle as pkl
+import numpy as np
+from psbody.mesh import Mesh
+import torch
+from libsmpl.smplpytorch.pytorch.smpl_layer import SMPL_Layer
+
+seq_root = "ROOT" # replace this with the root path of behave sequences
+frame = "Date01_Sub01_chairwood_hand/t0003.000"
+param_file = f"{seq_root}/{frame}/person/fit02/person_fit.pkl"
+mesh_file = f"{seq_root}/{frame}/person/fit02/person_fit.ply"
+gender = 'male' # gender information can be found in SEQ_PATH/info.json
+
+# path to SMPL-H model (MANOv1.2), see https://github.com/bharat-b7/MPI_MeshRegistration/tree/main/smpl_registration#smplh-files
+model_root = "ROOT"
+
+smpl_dict = pkl.load(open(param_file, 'rb'))
+p, b, t = smpl_dict['pose'], smpl_dict['betas'], smpl_dict['trans']
+pose = torch.tensor([p])
+betas = torch.tensor([b])
+trans = torch.tensor([t])
+
+smpl = SMPL_Layer(center_idx=0, gender=gender, num_betas=10,
+                               model_root=str(model_root), hands=True)
+verts, _, _, _ = smpl(pose, th_betas=betas, th_trans=trans)
+verts = verts[0].cpu().numpy()
+faces = smpl.th_faces.cpu().numpy()
+
+mesh = Mesh()
+mesh.load_from_file(mesh_file)
+
+
+err = np.sum((mesh.v-verts)**2)
+assert err < 1e-8, f'err={err}'
+print('all done')
+
+
